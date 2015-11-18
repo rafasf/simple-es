427a244c12c958333146c7146883385d69c4cc3a
Rafael Ferreira <rafaelsf@gmail.com>
2015-11-18 13:03:03 -0500

    Store an arbritary event

--- src/simple_es/core.clj
+++ src/simple_es/core.clj
@@ -1,5 +1,9 @@
 (ns simple-es.core)
 
+(defn has-keys [required-keys actual-keys]
+  (let [actual-required-keys (filter #(.contains required-keys %) actual-keys)]
+    (= required-keys actual-required-keys)))
+
 (defn reconstruct-from [events aggregate]
   (if (empty? events)
     aggregate
@@ -11,3 +15,5 @@
 (defn events-for [aggregate-id events]
   (filter #(= aggregate-id (:id %)) events))
 
+(defn store-event [event events]
+  (conj events event))
--- test/simple_es/core_test.clj
+++ test/simple_es/core_test.clj
@@ -14,4 +14,11 @@
     (is (= { :id "1" :name "one" :birth "1966-11-11" :last-name "three" :status "married" }
            (replay (events-for "1" events)))))
 
+  (deftest stores-an-event
+    (let [id "1"
+          new-event { :id id :name "blah" }
+          this-events (store-event new-event [])]
+      (is (= new-event
+             (replay (events-for id this-events))))))
+
   )
