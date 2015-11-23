0e9eca28a2eb31d7ad3b7f1540deee879053f5ce
Rafael Ferreira <rafaelsf@gmail.com>
2015-11-23 21:45:02 -0200

    Random refactoring... :P

--- src/simple_es/event.clj
+++ src/simple_es/event.clj
@@ -1,5 +1,10 @@
 (ns simple-es.event
-  (:require [simple-es.event-store :as events]))
+  (:require [taoensso.timbre :as timbre]
+            [clj-time.core :as t]
+            [clj-time.coerce :as tc]))
+
+;; fake store
+(def all-events (atom []))
 
 (defn reconstruct-from [events aggregate]
   (if (empty? events)
@@ -8,3 +13,22 @@
 
 (defn replay [events]
   (reconstruct-from (rest events) (first events)))
+
+(defn create-from [action fact]
+  (assoc action
+         :fact fact
+         :timestamp (tc/to-long (t/now))))
+
+(defn given [aggregate-id]
+  (filter #(= aggregate-id (:id %)) @all-events))
+
+(defn store [event]
+  (swap! all-events conj event)
+  (timbre/info "event(" (:fact event) ") tid(" (:transaction_id event) ")")
+  event)
+
+(defn all []
+  @all-events)
+
+(defn clean []
+  (reset! all-events []))
--- test/simple_es/event_test.clj
+++ test/simple_es/event_test.clj
@@ -1,17 +1,37 @@
 (ns simple-es.event-test
   (:require [clojure.test :refer :all]
-            [simple-es.event :refer :all]))
+            [simple-es.event :refer :all]
+            [clojure.test.check.generators :as gen]))
 
-(def events
-  [ { :id "1" :name "one" :birth "1900-11-11" :last-name "two" :status "single" }
-    { :id "1" :birth "1966-11-11" }
-    { :id "1" :status "married" :last-name "three" }
-    { :id "2" :status "single" } ])
+(defn a-raw-event []
+  (gen/generate (gen/hash-map :id gen/uuid :transaction_id gen/uuid :name gen/string-ascii :price gen/nat)))
 
-(defn events-for [id events]
-  (filter #(= id (:id %)) events))
+(defn an-event-from [event]
+  (create-from event (rand-nth [:happened-1 :happened-2 :happened-3])))
 
-(testing "events"
-  (deftest reconstructs-by-id
-    (is (= { :id "1" :name "one" :birth "1966-11-11" :last-name "three" :status "married" }
-           (replay (events-for "1" events))))))
+(testing "events replays"
+  (deftest replays-a-list-of-events
+    (let [predictable-events [{ :id "1" :name "one" :birth "1900-11-11" :last-name "two" :status "single" }
+                              { :id "1" :birth "1966-11-11" }
+                              { :id "1" :status "married" :last-name "three" }]]
+      (is (= { :id "1" :name "one" :birth "1966-11-11" :last-name "three" :status "married" }
+             (replay predictable-events))))))
+
+(testing "event creation"
+  (deftest creates-with-metadata
+    (let [event (create-from { :f1 "v1" } :item-added)]
+     (is (= :item-added (:fact event)))
+     (is (instance? java.lang.Long (:timestamp event)))))
+
+  (deftest stores-a-raw-event-and-returns-such
+    (let [clean-store (clean)
+          new-event (an-event-from (a-raw-event))]
+      (is (= (store new-event) new-event))
+      (is (= (all) [new-event])))))
+
+(testing "events fetching"
+  (deftest returns-events-for-given-id
+    (let [clean-store (clean)
+          events (take 5 (repeatedly (fn [] (store (an-event-from (a-raw-event))))))
+          random-event (rand-nth events)]
+      (is (= (given (:id random-event)) [random-event])))))
